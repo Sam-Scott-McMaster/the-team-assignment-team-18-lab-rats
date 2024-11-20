ff7889a14690ea90f3c382c963c91c21f132485e
Ashley Exall <exalla@mcmaster.ca>
2024-11-20 12:43:54 -0500

    Can add courses

---
+++ wanki-menu.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Create base directory 
+BASE_DIR="$HOME/WANKI"
+mkdir -p "$HOME/WANKI"
+
+main_menu(){
+    echo "Welcome to WANKI!"
+    echo "Select a course or add a new one:"
+
+    ## List current courses
+    local i=1
+    courses=()
+
+    for course_dir in "$BASE_DIR"/*/; do
+        course_name=$(basename "$course_dir")
+        echo "($i) $course_name"
+        courses[$i]=$course_name
+        ((i++))
+    done
+
+    # Option to add new course
+    echo "($i) Add new course"
+    echo "($((i+1))) Exit"
+    read -p "Enter choice: " choice
+
+    if (( choice == i )); then
+        create_course
+    elif (( choice == i+1)); then
+        exit 0
+    elif (( choice >= 1 && choice < i )); then
+        selected_course="${courses[$choice]}"
+        course_menu "$selected_course"
+    else
+        echo "Invalid option. Returning to main menu."
+    fi
+}
+
+
+# Function to create a new course
+create_course() {
+    read -p "Enter the new course name: " course_name
+    course_dir="$BASE_DIR/$course_name"
+    
+    if [ -d "$course_dir" ]; then
+        echo "Course '$course_name' already exists."
+    else
+        mkdir -p "$course_dir"
+        echo "Course '$course_name' created."
+    fi
+}
+
+while true; do
+    main_menu
+done
