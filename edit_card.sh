82abbe18ff15d14e4fec8356cd23712777b262c9
Kaiden <jessanik@mcmaster.ca>
2024-11-20 13:09:21 -0500

    edit_cards

--- edit_card.sh
+++ edit_card.sh
@@ -1,51 +1,42 @@
 #!/bin/bash
 
-echo "What would you like to edit, a question or an answer? "
-read Q_or_A
-if [[ $Q_or_A == "question" ]]
-then
-    edit_question()
-elif [[ $Q_or_A == "answer" ]]
-then
-    edit_answer()
-elif [[ $Q_or_A == "both" ]]
-then
-    edit_question()
-    edit_answer()
-fi    
-
-edit_question()
-{
-    echo "Type the original question, then hit enter"
+edit_question() {
+    echo "Type the original question, then hit enter:"
     read OG_QUESTION
-    question = ${cat "flashcards.txt" | grep "${OG_QUESTION}? |"  | wc -l}
-    if(question -eq 0)
-    then
+    question=$(grep -c "${OG_QUESTION}" flashcards.txt)
+    if [[ $question -eq 0 ]]; then
         echo "ERROR: QUESTION NOT FOUND"
-        echo $question
-        return EXIT_FAILURE
+        return 1
     else
-        echo "Type the new question, then hit enter"
+        echo "Type the new question, then hit enter:"
         read NEW_QUESTION
-        #remove term
-        #add term back with new question
+        sed -i "s/${OG_QUESTION}/${NEW_QUESTION}/1" flashcards.txt && echo "Question updated successfully."
     fi
 }
 
-edit_answer()
-{
-    echo "Type the original answer, then hit enter"
+edit_answer() {
+    echo "Type the original answer, then hit enter:"
     read OG_ANSWER
-    answer = ${cat "flashcards.txt" | grep "| $OG_ANSWER" | wc -l}
-    if(answer -eq 0)
-    then
+    answer=$(grep -c "${OG_ANSWER}" flashcards.txt)
+    if [[ $answer -eq 0 ]]; then
         echo "ERROR: ANSWER NOT FOUND"
-        echo $answer
-        return EXIT_FAILURE
+        return 1
     else
-        echo "Type the new answer, then hit enter"
+        echo "Type the new answer, then hit enter:"
         read NEW_ANSWER
-        #remove term
-        #add term back with new answer 
+        sed -i "s/${OG_ANSWER}/${NEW_ANSWER}/1" flashcards.txt && echo "Answer updated successfully."
     fi
 }
+
+echo "What would you like to edit, a question or an answer?"
+read Q_or_A
+if [[ $Q_or_A == "question" ]]; then
+    edit_question
+elif [[ $Q_or_A == "answer" ]]; then
+    edit_answer
+elif [[ $Q_or_A == "both" ]]; then
+    edit_question
+    edit_answer
+else
+    echo "Invalid option. Please type 'question', 'answer', or 'both'."
+fi
